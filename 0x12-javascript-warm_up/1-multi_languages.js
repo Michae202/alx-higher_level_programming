a3cc316b4b7019bfbd19598b35afd238ec88207e
Michae202 <mikewachirabb495@gmail.com>
2023-06-12 23:29:45 -0700

    1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,2 @@
 #!/usr/bin/node
-const myC = 'C is fun';
-const myPy = 'Python is cool';
-const myJs = 'JavaScript is amazing';
-console.log(myC);
-console.log(myPy);
-console.log(myJs);
+console.log('C is fun\nPython is cool\nJavaScript is amazing');
